eddd37c23c0ad13bc2e8bc367b899754a6fec09d
huyinghuan <ec.huyinghuan@gmail.com>
2013-01-03 17:10:25 -0800

    Create PDFShow.java
    servlet展示PDF

---
+++ PDFShow.java
@@ -0,0 +1,61 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PdfShow
+ */
+public class PdfShow extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PdfShow() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//ApplicationContext ac = new FileSystemXmlApplicationContex("applicationContext.xml");
+	   //(DAO) ac.getBean("beanId");		
+		File file = new File("f:\\s.pdf");
+		response.setContentType("application/pdf");
+		response.setDateHeader("expires", -1);
+		response.setHeader("Cache-Control", "no-cache");
+		response.setHeader("Pragma","no-cache");
+		OutputStream os = response.getOutputStream();
+		BufferedInputStream in = null;
+		try{
+			in  = new BufferedInputStream(new FileInputStream(file));
+			byte[] buffer = new byte[1024];
+			int len = 0;
+			while ((len = in.read(buffer)) > 0) {
+				os.write(buffer, 0, len);
+			}
+			in.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request,response);
+	}
+
+}
